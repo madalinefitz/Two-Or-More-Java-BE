f1581fccc5105f0bf4ca8330cc20a0c301c32830
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-17 16:13:43 -0400

    config and vocabulary entity updates with one value inserted to table

--- src/main/java/com/example/infoservice/controller/VocabularyController.java
+++ src/main/java/com/example/infoservice/controller/VocabularyController.java
@@ -4,6 +4,10 @@ import com.example.infoservice.service.VocabularyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 @RestController
 @RequestMapping("/vocabularies")
 public class VocabularyController {
@@ -11,9 +15,15 @@ public class VocabularyController {
     private final VocabularyService vocabularyService;
 
     @Autowired
-    public VocabularyController(VocabularyService vocabularyService) {
-        this.vocabularyService = vocabularyService;
+    public VocabularyController(VocabularyService service) {
+        this.vocabularyService = service;
     }
 
+    @GetMapping("/users")
+    public List<String> getUsers() {
+       return vocabularyService.GetVocabulary();
+    }
 }
 
+//controllers should only be your access point
+//controllers also responsible for security
--- src/main/java/com/example/infoservice/service/VocabularyService.java
+++ src/main/java/com/example/infoservice/service/VocabularyService.java
@@ -1,8 +1,12 @@
 package com.example.infoservice.service;
 
+import com.example.infoservice.entity.Vocabulary;
 import com.example.infoservice.repository.VocabularyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VocabularyService {
@@ -13,6 +17,18 @@ public class VocabularyService {
     public VocabularyService(VocabularyRepository vocabularyRepository) {
         this.vocabularyRepository = vocabularyRepository;
     }
+
+    public List<String> GetVocabulary() {
+        Vocabulary vocabulary = new Vocabulary();
+        vocabulary.setTerm("Brian");
+        vocabulary.setDefinition("great person");
+        Vocabulary vocabulary2 = new Vocabulary("foo", "this is the definition");
+        vocabularyRepository.saveAndFlush(vocabulary);
+        return new ArrayList<>();
+    }
+
+//    get the vocab table
+
 
 
 }
