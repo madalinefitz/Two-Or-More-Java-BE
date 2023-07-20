a29aa668a18cd832dc1ae0c37f74a9af169dbe67
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-07-20 14:02:22 -0400

    StoryService code added

--- src/main/java/com/example/infoservice/service/StoryService.java
+++ src/main/java/com/example/infoservice/service/StoryService.java
@@ -6,4 +6,19 @@ import org.springframework.stereotype.Service;
 public class StoryService {
 
     private final StoryRepository storyRepository;
+
+    @Autowired
+    public StoryService(StoryRepository storyRepository) {
+        this.stroyRepository = storyRepository;
+    }
+
+    public List<String> GetStory() {
+        Story story= new Story();
+        story.setStory("stories");
+        story.setFirstName("Cindy");
+        story.setLastInitial("L.");
+        Story story2 = new Story("stories", "Cindy", "L.");
+        storyRepository.saveAndFlush(story);
+        return new ArrayList<>();
+    }
 }
