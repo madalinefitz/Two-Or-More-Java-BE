e11c756563cb9095970ab6316f36605d945e30ca
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-07-20 13:52:34 -0400

    added StoryRepository

---
+++ src/main/java/com/example/infoservice/repository/StoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.infoservice.repository;
+
+import com.example.infoservice.entity.Story;
+import org.springframework.data.jpa.repository.JpaStory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoryRepository extends JpaRepository<Story, Integer> {
+    // You can define custom query methods or use the default methods provided by JpaRepository
+}
--- src/main/java/com/example/infoservice/service/StoryService.java
+++ src/main/java/com/example/infoservice/service/StoryService.java
@@ -4,4 +4,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StoryService {
+
+    private final StoryRepository storyRepository;
 }
