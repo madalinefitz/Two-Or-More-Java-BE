cbf426ab45f4278e5c9ce22cbdf166d7f8b94d42
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-25 09:23:57 -0400

    controller resolved

--- src/main/java/com/example/infoservice/controller/StoryController.java
+++ src/main/java/com/example/infoservice/controller/StoryController.java
@@ -1,7 +1,12 @@
 package com.example.infoservice.controller;
 
+import com.example.infoservice.service.StoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/stories")
